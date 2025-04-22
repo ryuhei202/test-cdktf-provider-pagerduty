424a7ec1a392779594f82c99503f45f8331a8da7
ryuhei202 <ryuhei202@docomo.ne.jp>
2025-04-22 18:55:35 +0900

    command: cdk init app --language typescript

---
+++ test/test-cdktf-provider-pagerduty.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestCdktfProviderPagerduty from '../lib/test-cdktf-provider-pagerduty-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test-cdktf-provider-pagerduty-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestCdktfProviderPagerduty.TestCdktfProviderPagerdutyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
